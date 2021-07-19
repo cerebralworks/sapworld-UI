d9c53b635471ce37c940202e46d097f44d4bdde3
ArunKumar S <arunkumar.s@agnitio-systems.com>
2021-07-19 11:59:46 +0530

    Code comments for the middleware

--- middleware/utils/utils.js
+++ middleware/utils/utils.js
@@ -1,15 +1,17 @@
 var moment = require('moment');
 
+/**
+**	To get the default contents
+**/
 var utils = {
-    loremIpsumGenerator: () => {
-            let text = "In a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual content still not being ready. Think of a news blog that's filled with content hourly on the day of going live. However, reviewers tend to be distracted by comprehensible content, say, a random text copied from a newspaper or the internet.";
-
-            return text;
-        },
+	loremIpsumGenerator: () => {
+		let text = "In a professional context it often happens that private or corporate clients corder a publication to be made and presented with the actual content still not being ready. Think of a news blog that's filled with content hourly on the day of going live. However, reviewers tend to be distracted by comprehensible content, say, a random text copied from a newspaper or the internet.";
+		return text;
+    },
     currentDate: () => {
-            let date = moment().format('MMMM Do YYYY, h:mm:ss a');
-            return date;
-        }
+		let date = moment().format('MMMM Do YYYY, h:mm:ss a');
+		return date;
+	}
 }
 
 export default utils;
