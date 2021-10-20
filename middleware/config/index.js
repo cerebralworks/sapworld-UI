ed887dd1b8f43d4bd917b7c748d0c21a71fdffcd
ArunKumar S <arunkumar.s@agnitio-systems.com>
2021-10-20 17:05:09 +0530

    Production Configuration

--- middleware/config/index.js
+++ middleware/config/index.js
@@ -1,48 +1,151 @@
 require("dotenv").config();
 const convict = require("convict");
 
-const config = convict({
-  NODE_ENV: {
-    format: ["production", "development", "test", "local", "stage"],
-    default: "development",
-    arg: "nodeEnv",
-    env: "NODE_ENV"
-  },
-  PORT: {
-    doc: "The port to bind.",
-    format: "port",
-    default: 4001,
-    env: "PORT",
-    arg: "port"
-  },
-  SERVICE_API_URL: {
-    doc: "The IP address to bind.",
-    format: "url",
-    default: "localhost",
-    env: "SERVICE_API_URL"
-  },
-  SECRET: {
-    doc: "Secret used for session cookies and CSRF tokens",
-    format: "*",
-    default: "",
-    sensitive: true
-  },
-  CLIENT_ID: {
-    doc: "Secret used for session cookies and CSRF tokens",
-    format: "*",
-    default: "",
-    sensitive: true,
-    env: "CLIENT_ID"
-  },
-  CLIENT_SECRET: {
-    doc: "Secret used for session cookies and CSRF tokens",
-    format: "*",
-    default: "",
-    sensitive: true,
-    env: "CLIENT_SECRET"
-  },
-});
+/**
+**	Loads the env data
+**/
 
-config.validate({ allowed: "strict" });
+if(process.env.NODE_ENV=='production'){
+	console.log(process.env.NODE_ENV);
+	const config = convict({
+		NODE_ENV: {
+			format: ["production", "development", "test", "local", "staging"],
+			default: "production",
+			arg: "nodeEnv",
+			env: "PRODUCTION_NODE_ENV"
+		},
+		PORT: {
+			doc: "The port to bind.",
+			format: "port",
+			default: 5000,
+			env: "PRODUCTION_PORT",
+			arg: "port"
+		},
+		SERVICE_API_URL: {
+			doc: "The IP address to bind.",
+			format: "url",
+			default: "localhost",
+			env: "PRODUCTION_SERVICE_API_URL"
+		},
+		SECRET: {
+			doc: "Secret used for session cookies and CSRF tokens",
+			format: "*",
+			default: "",
+			sensitive: true
+		},
+		CLIENT_ID: {
+			doc: "Secret used for session cookies and CSRF tokens",
+			format: "*",
+			default: "",
+			sensitive: true,
+			env: "PRODUCTION_CLIENT_ID"
+		},
+		CLIENT_SECRET: {
+			doc: "Secret used for session cookies and CSRF tokens",
+			format: "*",
+			default: "",
+			sensitive: true,
+			env: "PRODUCTION_CLIENT_SECRET"
+		},
+	});
+	
+	config.validate({ allowed: "strict" });
 
-module.exports = config.getProperties();
+	module.exports = config.getProperties();
+	
+}else if(process.env.NODE_ENV=='staging'){
+	console.log(process.env.NODE_ENV);
+	const config = convict({
+		NODE_ENV: {
+			format: ["production", "development", "test", "local", "staging"],
+			default: "staging",
+			arg: "nodeEnv",
+			env: "STAGE_NODE_ENV"
+		},
+		PORT: {
+			doc: "The port to bind.",
+			format: "port",
+			default: 4001,
+			env: "STAGE_PORT",
+			arg: "port"
+		},
+		SERVICE_API_URL: {
+			doc: "The IP address to bind.",
+			format: "url",
+			default: "localhost",
+			env: "STAGE_SERVICE_API_URL"
+		},
+		SECRET: {
+			doc: "Secret used for session cookies and CSRF tokens",
+			format: "*",
+			default: "",
+			sensitive: true
+		},
+		CLIENT_ID: {
+			doc: "Secret used for session cookies and CSRF tokens",
+			format: "*",
+			default: "",
+			sensitive: true,
+			env: "STAGE_CLIENT_ID"
+		},
+		CLIENT_SECRET: {
+			doc: "Secret used for session cookies and CSRF tokens",
+			format: "*",
+			default: "",
+			sensitive: true,
+			env: "STAGE_CLIENT_SECRET"
+		},
+	});
+	
+	config.validate({ allowed: "strict" });
+
+	module.exports = config.getProperties();
+	
+}else{
+	
+	const config = convict({
+		NODE_ENV: {
+			format: ["production", "development", "test", "local", "staging"],
+			default: "development",
+			arg: "nodeEnv",
+			env: "NODE_ENV"
+		},
+		PORT: {
+			doc: "The port to bind.",
+			format: "port",
+			default: 4001,
+			env: "PORT",
+			arg: "port"
+		},
+		SERVICE_API_URL: {
+			doc: "The IP address to bind.",
+			format: "url",
+			default: "localhost",
+			env: "SERVICE_API_URL"
+		},
+		SECRET: {
+			doc: "Secret used for session cookies and CSRF tokens",
+			format: "*",
+			default: "",
+			sensitive: true
+		},
+		CLIENT_ID: {
+			doc: "Secret used for session cookies and CSRF tokens",
+			format: "*",
+			default: "",
+			sensitive: true,
+			env: "CLIENT_ID"
+		},
+		CLIENT_SECRET: {
+			doc: "Secret used for session cookies and CSRF tokens",
+			format: "*",
+			default: "",
+			sensitive: true,
+			env: "CLIENT_SECRET"
+		},
+	});
+	config.validate({ allowed: "strict" });
+	module.exports = config.getProperties();
+	
+}
+
