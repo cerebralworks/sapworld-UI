fa67f053bf0aa51303b5ab328a9449956f77c210
ArunKumar S <arunkumar.s@agnitio-systems.com>
2021-10-21 11:56:05 +0530

    Environment port changing

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ console.log(baseUrl);
 
 export const environment = {
   production: true,
-  serverUrl: 'http://52.204.92.226:5000',
+  serverUrl: 'http://52.204.92.226:5002',
   adminUrl: 'http://184.72.227.41',
   envName: 'PROD',
   API_URL: 'assets/api',
