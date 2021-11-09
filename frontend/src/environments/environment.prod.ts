0f91c752a811ddbbf65b208c2e8a43b2e2b78168
ArunKumar <arunkumar.s@agnitio-systems.com>
2021-11-09 12:17:35 +0530

    Environment path changing

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 let host = window.location.host;
 let hostName = host.split(':');
 const baseUrl = `${window.location.protocol}//${hostName[0]}`;
+const baseAdminUrl = `${window.location.protocol}//admin.${hostName[0]}`;
 console.log(baseUrl);
 
 export const environment = {
   production: true,
-  serverUrl: 'http://sapworld.io:5002',
-  adminUrl: 'http://admin.sapworld.io',
+  serverUrl: `${baseUrl}:5002`,
+  adminUrl: `${baseAdminUrl}`,
   envName: 'PROD',
   API_URL: 'assets/api',
   clientUrl: `${baseUrl}`
