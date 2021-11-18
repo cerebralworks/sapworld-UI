fe75a80adcd9606cfa1ad924ecb685a1e8b01cd7
ArunKumar <arunkumar.s@agnitio-systems.com>
2021-11-18 16:03:20 +0530

    Image to text changing

--- frontend/src/app/modules/auth/employer-auth/employer-login/employer-login.component.ts
+++ frontend/src/app/modules/auth/employer-auth/employer-login/employer-login.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit,HostListener  } from '@angular/core';
 export {}; declare global { interface Window { Parallax: any; } } 
-import { TranslateService,LangChangeEvent } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-employer-login',
@@ -10,8 +9,7 @@ import { TranslateService,LangChangeEvent } from '@ngx-translate/core';
 export class EmployerLoginComponent implements OnInit {
 public screenWidth: any;
 public translatePage='en';
-  constructor(
-    public translate: TranslateService) { }
+  constructor() { }
 
   ngOnInit(): void {
 	  this.screenWidth = window.innerWidth;
@@ -20,19 +18,6 @@ public translatePage='en';
 			new window.Parallax(scene)
 	  });
 	  
-	  this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
-		if(event.lang=='es'){
-			this.translatePage = 'es';
-		}if(event.lang=='de'){
-			this.translatePage = 'de';
-		}if(event.lang=='en'){
-			this.translatePage = 'en';
-		}
-		setTimeout(() => {
-			  var scene = document.getElementById('scene');
-				new window.Parallax(scene)
-		  });
-	});
   }
   /* ngAfterViewInit(): void {
 	  setTimeout(() => {
