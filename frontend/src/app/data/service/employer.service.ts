9d6fe03d8910b1ae74c5ae13cea75ef04a16de1d
ArunKumar <arunkumar.s@agnitio-systems.com>
2021-11-19 11:11:51 +0530

    Employer service addding

--- frontend/src/app/data/service/employer.service.ts
+++ frontend/src/app/data/service/employer.service.ts
@@ -18,6 +18,19 @@ export class EmployerService extends CacheService {
     super();
   }
   
+  
+   /**
+   * To create a new contact form details
+   */
+   
+  createcontact = (data) => {
+    return this.apiService.post('/api/contact/create',data).pipe(
+      map(data => {
+        return data;
+      })
+    );
+  };
+  
   /**
    * To update the employer profile details
    */
