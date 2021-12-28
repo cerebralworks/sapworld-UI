0a52cbb5e28d6da833ea77f1905d4ba976c23cb4
ArunKumar <arunkumar.s@agnitio-systems.com>
2021-12-28 12:33:56 +0530

    Bug Fixing with the login flow issue

--- frontend/src/app/modules/auth/employer-auth/employer-login/employer-login.component.ts
+++ frontend/src/app/modules/auth/employer-auth/employer-login/employer-login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit,HostListener  } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoggedIn } from '@data/schema/account';
+import { AccountService } from '@data/service/account.service';
+
 export {}; declare global { interface Window { Parallax: any; } } 
 
 @Component({
@@ -9,9 +13,22 @@ export {}; declare global { interface Window { Parallax: any; } }
 export class EmployerLoginComponent implements OnInit {
 public screenWidth: any;
 public translatePage='en';
-  constructor() { }
+  public loggedUserInfo: LoggedIn;
+  constructor(
+    private router: Router,
+    private accountService: AccountService) { }
 
   ngOnInit(): void {
+	  this.accountService
+	  .isCurrentUser()
+	  .subscribe(response => {
+		this.loggedUserInfo = response;
+		if(response.role.includes(1)){
+			this.router.navigate(['/employer/dashboard']);
+		}else if(response.role.includes(0)){
+			this.router.navigate(['/user/dashboard']);
+		}
+	  });
 	  this.screenWidth = window.innerWidth;
 	  setTimeout(() => {
 		  var scene = document.getElementById('scene');
