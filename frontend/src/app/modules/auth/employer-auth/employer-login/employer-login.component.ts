bd3de8197f016e550a2089b24bb481ffe044b4e6
ArunKumar <arunkumar.s@agnitio-systems.com>
2021-12-28 15:19:20 +0530

    Bug Fixing

--- frontend/src/app/modules/auth/employer-auth/employer-login/employer-login.component.ts
+++ frontend/src/app/modules/auth/employer-auth/employer-login/employer-login.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit,HostListener  } from '@angular/core';
-import { Router } from '@angular/router';
-import { LoggedIn } from '@data/schema/account';
-import { AccountService } from '@data/service/account.service';
-
 export {}; declare global { interface Window { Parallax: any; } } 
 
 @Component({
@@ -13,22 +9,9 @@ export {}; declare global { interface Window { Parallax: any; } }
 export class EmployerLoginComponent implements OnInit {
 public screenWidth: any;
 public translatePage='en';
-  public loggedUserInfo: LoggedIn;
-  constructor(
-    private router: Router,
-    private accountService: AccountService) { }
+  constructor() { }
 
   ngOnInit(): void {
-	  this.accountService
-	  .isCurrentUser()
-	  .subscribe(response => {
-		this.loggedUserInfo = response;
-		if(response.role.includes(1)){
-			this.router.navigate(['/employer/dashboard']);
-		}else if(response.role.includes(0)){
-			this.router.navigate(['/user/dashboard']);
-		}
-	  });
 	  this.screenWidth = window.innerWidth;
 	  setTimeout(() => {
 		  var scene = document.getElementById('scene');
