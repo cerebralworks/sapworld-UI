345ee1aba6c488e012defa9f50a52d524b34e9cf
karthikeyan <karthikeyan.n@agnitio-systems.com>
2022-04-28 10:25:42 +0530

    safari revet

--- frontend/src/app/modules/features/employer/post-job/post-job.module.ts
+++ frontend/src/app/modules/features/employer/post-job/post-job.module.ts
@@ -12,7 +12,7 @@ import { JobPreviewComponent } from './modal/job-preview/job-preview.component';
 import { SharedModule } from '@shared/shared.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MaterialModule } from '@shared/material.module';
-import { AmazingTimePickerModule } from 'amazing-time-picker';
+//import { AmazingTimePickerModule } from 'amazing-time-picker';
 
 const routes: Routes = [
   {
@@ -38,8 +38,8 @@ const routes: Routes = [
     MaterialModule,
     NgbModule,
     RouterModule.forChild(routes),
-    SharedModule,
-	AmazingTimePickerModule 
+    SharedModule
+	//AmazingTimePickerModule 
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA],
   exports: [RouterModule]
