a4649223ebfb0776514fc109c3543f1310725caa
Vishal M <vishal.m@agnitio-systems.com>
2022-08-22 18:52:52 +0530

    debuuging

--- frontend/src/app/modules/features/common/landing/landing.component.ts
+++ frontend/src/app/modules/features/common/landing/landing.component.ts
@@ -22,7 +22,7 @@ import { catchError, debounceTime, distinctUntilChanged, filter, map, switchMap 
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent extends CacheService implements OnInit, AfterViewInit {
+export class LandingComponent extends CacheService implements OnInit {
 
   public isFindSearch: any = 0;
   public filteredJobs: any;
@@ -84,248 +84,19 @@ export class LandingComponent extends CacheService implements OnInit, AfterViewI
     super();
   }
 
-  onCheck = (event) => {
-console.log(event);
-  }
-	
 	/**
    * To initialize the page
    */
 	ngOnInit(): void {
-		this.onCreateForm();
-		this.accountService
-		  .isCurrentUser()
-		  .subscribe(response => {
-			this.loggedUserInfo = response;
-		  });
+		
 	}
 	
-	/**
-   * TO create a 
-   */
-  onCreateForm = () => {
-    this.searchForm = this.formBuilder.group({
-      search: new FormControl(''),
-      location: new FormControl(''),
-      skillId: new FormControl('')
-    }, { validator: atLeastOne(Validators.required, ['search', 'location']) });
-  }
-
-  atLeastOnePhoneRequired(group: FormGroup): { [s: string]: boolean } {
-    if (group) {
-      if (group.controls['search'].value || group.controls['location'].value) {
-        return null;
-      }
-    }
-    return { 'error': true };
-  }
-
-  get f() {
-    return this.searchForm.controls;
-  }
-
-  ngAfterViewInit(): void {
-    // this.onTitleSearch();
-    this.loadMovies();
-    this.changeDetectorRef.detectChanges();
-  }
-
-  loadMovies() {
-
-    this.movies$ = concat(
-      of([]), // default items
-      this.moviesInput$.pipe(
-        filter(res => {
-          return res !== null && res.length >= this.minLengthTerm
-        }),
-        distinctUntilChanged(),
-        debounceTime(800),
-        switchMap(term => {
-
-          return this.getMovies(term).pipe(
-            catchError(() => of([])), // empty list on error
-          )
-        })
-      )
-    );
-
-  }
-
-  trackByFn(item: any) {
-    return item.id;
-  }
-
-  getMovies(term: string = null): Observable<any> {
-    this.filteredValues = [];
-    //this.filteredValuesCountry = [];
-    let requestParams: any = {};
-    requestParams.page = 1;
-    requestParams.limit = 1000;
-    requestParams.status = 1;
-    requestParams.search = term;
-    return this.employerService.getSkill(requestParams).pipe(
-      map((response) => {
-          if (response && response.items && response.items.length > 0) {
-           return [...response.items];
-          }
-        }, error => {
-        }
-      )
-    )
-    // onGetSkillTag
-    // return this.http
-    //   .get<any>('http://www.omdbapi.com/?apikey=[YOUR_OMDB_KEY]&s=' + term)
-    //   .pipe(map(resp => {
-    //     if (resp.Error) {
-    //       throwError(resp.Error);
-    //     } else {
-    //       return resp.Search;
-    //     }
-    //   })
-    //   );
-  }
-
-
-  onTitleSearch = () => {
-    if (this.searchJobs && this.searchJobs.nativeElement) {
-      fromEvent(this.searchJobs.nativeElement, 'keyup').pipe(
-        map((event: any) => {
-          return event.target.value;
-        })
-        // if character length greater then 2
-        , filter(res => res.length > 1)
-        // Time in milliseconds between key events
-        , debounceTime(600)
-        // If previous query is diffent from current
-        , distinctUntilChanged()
-        // subscription for response
-      ).subscribe((text: string) => {
-        // if (this.loggedUserInfo.isLoggedIn && this.loggedUserInfo.role.includes(1)) {
-        //   this.onGetSkillTag(text);
-        // } else {
-        //   this.onGetJobBySearch(text)
-        // }
-        this.onGetSkillTag(text);
-      });
-    }
-
-  }
-
-  onGetJobBySearch = (query) => {
-    this.filteredValues = [];
-    let requestParams: any = {};
-    requestParams.page = 1;
-    requestParams.limit = 1000;
-    requestParams.search = query;
-    this.employerService.getPostedJob(requestParams).subscribe(
-      (response: any) => {
-        if (response && response.items && response.items.length > 0) {
-          this.filteredValues = [...response.items];
-        }
-      },
-      (error) => {
-        this.filteredValues = [];
-      }
-    )
-  }
-
+	
+ 
 
   
-  onGetSkillTag(query) {
-    this.filteredValues = [];
-    let requestParams: any = {};
-    requestParams.page = 1;
-    requestParams.limit = 1000;
-    requestParams.status = 1;
-    requestParams.search = query;
-
-    this.employerService.getSkill(requestParams).subscribe(
-      response => {
-        if (response && response.items && response.items.length > 0) {
-          this.filteredValues = [...response.items];
-        }
-      }, error => {
-      }
-    )
-  }
-
-  onSelectSkill = (item) => {
-    this.selectedSkillItem = item;
-    if(this.loggedUserInfo.isLoggedIn && this.loggedUserInfo.role.includes(1)) {
-      this.setItem('homeSelectedSkill', item);
-      this.searchForm.patchValue({
-        search: item.tag ? item.tag.toString() : this.searchForm.value.search.toString(),
-        skillId: item.id ? item.id.toString() : ''
-      });
-    }else {
-      this.searchForm.patchValue({
-        search: item.title ? item.title.toLowerCase() : this.searchForm.value.search.toLowerCase()
-      });
-    }
-    this.filteredValues = []
-  }
-
-
-  handleAddressChange = (event) => {
-    const address = this.sharedService.fromGooglePlace(event);
-    if (address && address.city) {
-      this.location = address.city.toLowerCase();
-    } else if (address && address.state) {
-      this.location = address.state.toLowerCase();
-    }
-  };
-
-  onRedirect = () => {
-    if (this.searchForm.valid) {
-      if(this.loggedUserInfo.isLoggedIn && this.loggedUserInfo.role.includes(1)) {
-        this.setItem('homeSelectedSkill', this.onReturnIDFronArray(this.searchForm.value.search, 'tag', true));
-        this.router.navigate(['/find-candidates'],
-          {
-            queryParams:
-            {
-              skill_tags: this.onReturnIDFronArray(this.searchForm.value.search, 'id'),
-              city: (this.searchForm.value && this.searchForm.value.location) ? this.searchForm.value.location : ''
-            }
-          });
-
-      } else {
-        this.setItem('homeSelectedSkill', this.onReturnIDFronArray(this.searchForm.value.search, 'tag', true));
-        this.router.navigate(['/find-jobs'],
-          {
-            queryParams:
-            {
-              skills: this.onReturnIDFronArray(this.searchForm.value.search, 'id'),
-              city: (this.searchForm.value && this.searchForm.value.location) ? this.searchForm.value.location : ''
-            }
-          });
-      }
-    }
-  }
  onRedirectUrl = (url: string) => {
     this.router.navigate([url])
   }
   
-  onReturnIDFronArray = (arrayOfObj: any [] =[], field: string, isString: boolean = false) => {
-    if(Array.isArray(arrayOfObj) && arrayOfObj.length) {
-      return arrayOfObj.map((val) => {
-        return isString ? val[field] ? this.utilsHelperService.onSplitTag(val[field]) : '': val[field] ? val[field] : '';
-      }).toString();
-    }
-    return "";
-  }
-
-}
-
-export const atLeastOne = (validator: ValidatorFn, controls: string[] = null) => (
-  group: FormGroup,
-): ValidationErrors | null => {
-  if (!controls) {
-    controls = Object.keys(group.controls)
-  }
-
-  const hasAtLeastOne = group && group.controls && controls
-    .some(k => !validator(group.controls[k]));
-  return hasAtLeastOne ? null : {
-    atLeastOne: true,
-  };
 };
