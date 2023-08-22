06b3d25fbaf562b4d601f4e0770d8eb2f41be446
Vishal M <vishal.m@agnitio-systems.com>
2023-08-22 19:20:27 +0530

    bug fix

--- frontend/src/app/modules/features/user/profile/create-candidate/create-candidate-layout/create-candidate-layout.component.ts
+++ frontend/src/app/modules/features/user/profile/create-candidate/create-candidate-layout/create-candidate-layout.component.ts
@@ -551,8 +551,9 @@ export class CreateCandidateLayoutComponent implements OnInit {
 
 	onUserPhotoUpdate = (candidateInfo) => {
 		const formData = new FormData();
-		var rString = this.randomString(40, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-		formData.append('photo' , this.userPhotoInfo.photoBlob, ((this.userPhotoInfo && this.userPhotoInfo.photoBlob && this.userPhotoInfo.photoBlob.name) ? this.userPhotoInfo.photoBlob.name : rString));
+		//var rString = this.randomString(40, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+		//formData.append('photo' , this.userPhotoInfo.photoBlob, ((this.userPhotoInfo && this.userPhotoInfo.photoBlob && this.userPhotoInfo.photoBlob.name) ? this.userPhotoInfo.photoBlob.name : rString));
+		formData.append('photo', base64ToFile(this.userPhotoInfo.photoBlob));
 		formData.append('extension' , this.userPhotoInfo.ext);
 		this.userService.photoUpdate(formData).subscribe(
 		response => {
